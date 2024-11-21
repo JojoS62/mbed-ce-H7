76c48f9c95ec7f599bb284e4d475e503922af7fd
Johannes Stratmann <sn2010@onlinehome.de>
2024-11-21 18:41:07 +0100

    add test-sdio app

---
+++ app_test_sdio/sdram.h
@@ -0,0 +1,37 @@
+#ifndef __USER_SDRAM__H__
+#define __USER_SDRAM__H__
+
+#include "mbed.h"
+#include "fmc.h"
+
+#define SDRAM_Size                (32 * 1024 * 1024)
+#define SDRAM_BANK_ADDR           ((uint32_t)0xC0000000)
+#define FMC_COMMAND_TARGET_BANK   FMC_SDRAM_CMD_TARGET_BANK1	//	SDRAM bank selection
+#define SDRAM_TIMEOUT             ((uint32_t)0x1000) 			//  timeout
+
+#define SDRAM_MODEREG_BURST_LENGTH_1             ((uint16_t)0x0000)
+#define SDRAM_MODEREG_BURST_LENGTH_2             ((uint16_t)0x0001)
+#define SDRAM_MODEREG_BURST_LENGTH_4             ((uint16_t)0x0002)
+#define SDRAM_MODEREG_BURST_LENGTH_8             ((uint16_t)0x0004)
+#define SDRAM_MODEREG_BURST_TYPE_SEQUENTIAL      ((uint16_t)0x0000)
+#define SDRAM_MODEREG_BURST_TYPE_INTERLEAVED     ((uint16_t)0x0008)
+#define SDRAM_MODEREG_CAS_LATENCY_2              ((uint16_t)0x0020)
+#define SDRAM_MODEREG_CAS_LATENCY_3              ((uint16_t)0x0030)
+#define SDRAM_MODEREG_OPERATING_MODE_STANDARD    ((uint16_t)0x0000)
+#define SDRAM_MODEREG_WRITEBURST_MODE_PROGRAMMED ((uint16_t)0x0000)
+#define SDRAM_MODEREG_WRITEBURST_MODE_SINGLE     ((uint16_t)0x0200)
+
+
+void SDRAM_Initialization_Sequence(SDRAM_HandleTypeDef *hsdram);
+HAL_StatusTypeDef SDRAM_Test(void);
+
+
+#endif  //!__USER_SDRAM__H__
+
+
+
+
+
+
+
+
